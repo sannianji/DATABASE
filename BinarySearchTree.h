c838dee357adc5a3f7f35807ce31990323adaefe
StormLu <sannianjisansan@gmail.com>
2017-02-26 19:19:14 +0800

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,6 +1,8 @@
 #pragma once
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
+//重点利用递归解决问题，注意模板的使用过程中对于typename的添加，切记
+//节点
 template<typename Comparable>
 struct BinaryNode
 {
@@ -14,25 +16,30 @@ template<typename Comparable>
 class BinarySearchTree
 {
 public:
+	//三大函数
 	BinarySearchTree();
 	BinarySearchTree(const BinarySearchTree&);
 	~BinarySearchTree();
-
+public:
+	//接口只读函数
 	const Comparable& findMin() const;
 	const Comparable& findMax() const;
 	bool contains(const Comparable & x)const;
 	bool isEmpty()const;
 	void printTree()const;
-
+	//接口读写函数
 	void makeEmpty();
 	void insert(const Comparable & x);
 	void remove(const Comparable & x);
-
+public:
+	//重载操作符
 	const BinarySearchTree & operator=(const BinarySearchTree & rhs);
 
 private:
+	//数据
 	BinaryNode<Comparable>* root;
-
+private:
+	//重点实现函数
 	void insert(const Comparable & x, BinaryNode<Comparable> * & t);
 	void remove(const Comparable & x, BinaryNode<Comparable> * & t);
 	BinaryNode<Comparable> * findMin(BinaryNode<Comparable> * t)const;
