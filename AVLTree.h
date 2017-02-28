351b1c497db202b95d85278303317d348a422954
sannianji <sannianjisansan@gmail.com>
2017-03-01 00:06:47 +0800

    A

--- AVLTree.h
+++ AVLTree.h
@@ -1,59 +1,58 @@
 #ifndef AVL_TREE_H
 #define AVL_TREE_H
-template<typename Element>
+#include <iostream>
+template <typename Element>
 struct AVLNode
 {
-	//class functions
-	AVLNode(const Element & el,AVLNode* lf=nullptr,AVLNode* rt=nullptr,int ht=0)
-	:LeftNode(lf),RightNode(rt),hight(0),Element(el){}
+    //class functions
+    AVLNode(const Element &el, AVLNode *lf = nullptr, AVLNode *rt = nullptr, int ht = 0)
+	: LeftNode(lf), RightNode(rt), hight(0), Element(el) {}
 
-	//data
-	AVLNode* LeftNode;
-	AVLNode* RightNode;
-	int hight;
-	Element Elements;
-	
-	//interface functions
-	int hightReturn(AVLNode* t)
-		{return (t==nullptr)?-1:t->hight;}		
+    //data
+    AVLNode *LeftNode;
+    AVLNode *RightNode;
+    int hight;
+    Element Elements;
+
+    //interface functions
+    int hightReturn(AVLNode *t)
+    {
+	return (t == nullptr) ? -1 : t->hight;
+    }
 };
-template<typename Element>
+template <typename Element>
 class AVLTree
 {
-public:
-	//class functions
-	AVLTree();
-	AVLTree(const AVLTree &);
-	~AVLTree();
+  public:
+    //class functions
+    AVLTree();
+    AVLTree(const AVLTree &);
+    ~AVLTree();
 
-	//interface functions for information
-	const Element& findMin()const;
-	const Element& findMax()const;
-	const int hightMax()const{return (root->heightReturn()==-1)?0:(root->heightReturn());};
-	std::ostream& print()const;
-	bool contains(const Element & )const;
+    //interface functions for information
+    const Element &findMin() const;
+    const Element &findMax() const;
+    const int hightMax() const { return (root->heightReturn() == -1) ? 0 : (root->heightReturn()); };
+    std::ostream &print() const;
+    bool contains(const Element &) const;
 
-	//interface functions can change the class
-	bool insert(const Element & );
-	bool remove(const Element & );
-	void clear();	
-	
-private:
-<<<<<<< HEAD
-	//data
-	AVLNode<Element>* root;
-=======
+    //interface functions can change the class
+    bool insert(const Element &);
+    bool remove(const Element &);
+    void clear();
 
-	AVLNode<Element>* Root;
->>>>>>> testing
-	int AVLTreehight;
-	//function
-	const AVLNode<Element>* findMin(const AVLNode<Element>*) const;
-	const AVLNode<Element>* findMax(const AVLNode<Element>*) const;
-	bool contains(const Element &,const AVLNode<Element>*) const;
-	
-	bool insert(const Element &, const AVLNode<Element>*&);
-	bool remove(const Element &, const AVLNode<Element>*&);
-	void clear(AVLNode<Element>*&);	
+  private:
+    //data
+    AVLNode<Element> *root;
+
+    int AVLTreehight;
+    //function
+    const AVLNode<Element> *findMin(const AVLNode<Element> *) const;
+    const AVLNode<Element> *findMax(const AVLNode<Element> *) const;
+    bool contains(const Element &, const AVLNode<Element> *) const;
+
+    bool insert(const Element &, const AVLNode<Element> *&);
+    bool remove(const Element &, const AVLNode<Element> *&);
+    void clear(AVLNode<Element> *&);
 };
 #endif
