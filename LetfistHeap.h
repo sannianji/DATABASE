e816a9d7acbaf9e151b00a1b9d56aad6ce042c4d
StormLu <sannianjisansan@gmail.com>
2017-03-08 23:06:02 +0800

    Update LetfistHeap.h

--- LetfistHeap.h
+++ LetfistHeap.h
@@ -2,6 +2,18 @@
 #ifndef LEFTIST_HEAP_H
 #define LEFTIST_HEAP_H
 template<typename Comparable>
+struct LeftistNode
+{
+	Comparable element;
+	LeftistNode * left;
+	LeftistNode * right;
+	int npl;
+
+	LeftNode(const Comparable & theElement, LeftistNode * lt = nullptr, LeftistNode* rt = nullptr, int np = 0)
+		:element(theElement), left(lt), right(rt), npl(np) {}
+
+};
+template<typename Comparable>
 class LeftistHeap
 {
 public:
@@ -15,82 +27,21 @@ public:
 	void insert(const Comparable & x);
 	void deleteMin();
 	void deleteMin(Comparable & minItem);
-	void makeEmty();
+	void makeEmpty();
 	void merge(LeftistHeap & rhs);
 
-	const LeftisHeap &operator=(const LeftisHeap& rhs);
+	const LeftistHeap &operator=(const LeftistHeap& rhs);
 private:
-	struct LeftistNode
-	{
-		Comparable element;
-		LeftistNode * left;
-		LeftistNode * right;
-		int npl;
-		
-		LeftNode(const Comparable & theElement, LeftistNode * lt = nullptr, LeftistNode* rt = nullptr, int np = 0)
-			:element(theElement), left(lt), right(rt), npl(np) {}
+	
+	//data
+	LeftistNode<Comparable> * root;
 
-	};
-	LeftistNode * root;
-	LeftistNode * merge(LeftistNode * h1, LeftistNode * h2);
-	LeftistNode * merge1(LeftistNode * h1, LeftistNode * h2);
-
-	void swapChildren(LeftistNode * t);
-	void reclaimMemory(LeftistNode * t);
-	LetfistNode * clone(LeftistNode * t)const;
+	LeftistNode<Comparable> * merge(LeftistNode<Comparable> * h1, LeftistNode<Comparable> * h2);
+	LeftistNode<Comparable> * merge1(LeftistNode<Comparable> * h1, LeftistNode<Comparable> * h2);
+	void * makeEmpty(LeftistNode<Comparable> * rhs);
+	
+	void swapChildren(LeftistNode<Comparable> * t);
+	void reclaimMemory(LeftistNode<Comparable> * t);
+	LeftistNode<Comparable> * clone(LeftistNode<Comparable> *)const;
 };
-template<typename Comparable>
-void LeftistHeap<Comparable>::merge(LeftistHeap & rhs)
-{
-	if (this == &rhs)
-		return;
-	root = merge(root, rhs.root);
-}
-template<typename Comparable>
-LeftistNode<Comparable> * LeftistHeap<Comparable>::merge(LeftistNode<Comparable> *h1, LeftistNode<Comparable> *h2)
-{
-	if (h1 == nullptr)
-		return h2;
-	if (h2 == nullptr)
-		return h1;
-	if (h1->element < h2->element)
-		return merge1(h1, h2);
-	else
-		return merge1(h2, h1);
-}
-template<typename Comparable>
-LeftistNode<Comparable> * LeftistHeap<Comparable>::merge1(LeftistNode<Comparable> * h1, LeftistNode<Comparable> * h2)
-{
-	if (h1->left = nullptr)
-		h1->left = h2;
-	else
-	{
-		h1->right = merge(h1->right, h2);
-		if (h1->left->npl < h1->right - npl)
-			swapChildren(h1);
-		h1->npl = h1->right->npl + 1;
-	}
-	return h1;
-}
-template<typename Comparable>
-void LeftistHeap<Comparable>::insert(const Comparable & x)
-{
-	root = merge(new LeftistNode<Comparable>(x), root);
-}
-template<typename Comparable>
-void LeftistHeap<Comparable>::deleteMin()
-{
-	if (isEmpty())
-		throw UnderflowException();
-	LeftistNode<Comparable> * oldRoot = root;
-	root = merge(root->left, root->right);
-	delete oldRoot;
-}
-template<typename Comparable>
-void deleteMin(Comparable & minItem)
-{
-	minItem = findMin();
-	deletMin();
-}
-
 #endif
